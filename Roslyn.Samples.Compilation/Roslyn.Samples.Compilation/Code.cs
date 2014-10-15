b5921191991ac84bc710e62dae72f693ca7204c8
filipw <wojcieszyn@gmail.com>
2014-10-15 17:30:36 +0200

    Added compilation sample

---
+++ Roslyn.Samples.Compilation/Roslyn.Samples.Compilation/Code.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Foo
+{
+    public static void Bar()
+    {
+        var items = new int[4] { 1, 2, 3, 4 };
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Roslyn.Samples.Compilation/Roslyn.Samples.Compilation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Roslyn.Samples.Compilation
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var code = File.ReadAllText("Code.cs");
+
+            var tree = SyntaxFactory.ParseSyntaxTree(code);
+
+            var compilation = CSharpCompilation.Create(
+                "test.dll",
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                syntaxTrees: new[] {tree},
+                references: new[]
+                {
+                    new MetadataFileReference(typeof (object).Assembly.Location),
+                });
+
+            using (var stream = new MemoryStream())
+            {
+                var compileResult = compilation.Emit(stream);
+                if (compileResult.Success)
+                {
+                    var compiledAssembly = Assembly.Load(stream.GetBuffer());
+                    var program = compiledAssembly.GetType("Foo");
+                    var entry = program.GetMethod("Bar");
+                    entry.Invoke(null, null);
+                }
+                else
+                {
+                    foreach (var diagnostic in compileResult.Diagnostics)
+                    {
+                        Console.WriteLine(diagnostic.ToString());
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
