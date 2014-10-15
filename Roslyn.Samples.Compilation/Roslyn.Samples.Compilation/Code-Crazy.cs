5d8e895059c59d1ffb0bf6adaea1118d6e99f4e2
filipw <wojcieszyn@gmail.com>
2014-10-15 17:50:01 +0200

    lol# sample code

---
+++ Roslyn.Samples.Compilation/Roslyn.Samples.Compilation/Code-Crazy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+OHAI LOL Foo
+{
+    OHAI STATIK void Bar() 
+    {
+         var items = GIMME int[5] {1, 2, 3, 4, 5};
+         EVRY (var item in items) 
+         {
+              System.Console.WriteLine(item);
+         }
+     }
+}
